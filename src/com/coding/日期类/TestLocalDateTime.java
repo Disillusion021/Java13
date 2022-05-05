8e87a5cd3e27b604e73a4340fa03567e226a5846
Disillusion021 <disillusion021@gmail.com>
2022-05-05 22:33:15 +0800

    第三代日期类LocalDateTime

---
+++ src/com/coding/日期类/TestLocalDateTime.java
@@ -0,0 +1,30 @@
+package com.coding.日期类;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
+public class TestLocalDateTime {
+    public static void main(String[] args) {
+        LocalDateTime ldt = LocalDateTime.now();
+        System.out.println(ldt);
+        System.out.println(ldt.getYear());
+        System.out.println(ldt.getMonthValue());
+        System.out.println(ldt.getMonth());
+        System.out.println(ldt.getDayOfMonth());
+        System.out.println(ldt.getHour());
+        System.out.println(ldt.getMinute());
+        System.out.println(ldt.getSecond());
+        // 格式化日期输出格式
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy年MM月dd日 HH小时mm分钟ss秒");
+        System.out.println(dateTimeFormatter.format(ldt));
+        // 第三代日期类还有一个类 叫 Instant 时间戳， 类似于第一代日期类 Date
+        // Instant -> Date
+        Instant now = Instant.now();
+        System.out.println(now);
+        Date date = Date.from(now);
+        // Date -> Instant
+        Instant instant = date.toInstant();
+    }
+}
