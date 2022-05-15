4ad9d8bcb57f647df25f52146259958ef62f8800
Disillusion021 <disillusion021@gmail.com>
2022-05-15 23:10:47 +0800

    Arraylist源码调试分析

--- src/com/coding/Test/集合/ArrayListSource.java
+++ src/com/coding/Test/集合/ArrayListSource.java
@@ -1,13 +1,13 @@
 package com.coding.Test.集合;
 
 import java.util.ArrayList;
-import java.util.Collection;
 
 // ArrayListSource源码解读
 public class ArrayListSource {
     public static void main(String[] args) {
+        // ArrayList 线程不安全但效率高，如果是有参构造按1.5倍扩容，如果是无参，则第一次是10，第二次开始按1.5倍扩容
         ArrayList list = new ArrayList();
-//    ArrayList list = new ArrayList(8);
+        // ArrayList list = new ArrayList(8);
         for (int i = 0; i < 11; i++) {
             list.add(i);
         }
