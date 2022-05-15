9566111aec649269ec8b2d04d7760022460a409c
Disillusion021 <disillusion021@gmail.com>
2022-05-15 15:58:19 +0800

    测试Iterator的使用

--- src/com/coding/Test/集合/CollectionIterator.java
+++ src/com/coding/Test/集合/CollectionIterator.java
@@ -28,6 +28,42 @@ public class CollectionIterator {
             System.out.println(o);
         }
 
+        // 测试for循环，iterator性能，构建测试用数组 start
+        for (int i = 0; i < 8000000; i++) {
+            col.add(new Book("红楼梦","曹雪芹",34.6));
+        }
+        long start;
+        long end;
+        // 第一种 for 循环
+        start = System.currentTimeMillis();
+        for (int i = 0; i < col.size(); i++) {
+            ;
+        }
+        end = System.currentTimeMillis();
+        System.out.println("循环" + col.size() + "此for循环用时:\t" + (end - start));
+        // 第二种 增强for 循环
+        start = System.currentTimeMillis();
+        for (Object o : col) {
+            ;
+        }
+        end = System.currentTimeMillis();
+        System.out.println("循环" + col.size() + "此增强for循环用时:\t" + (end - start));
+        // 第三种 for iterator 循环
+        start = System.currentTimeMillis();
+        for(Iterator it = col.iterator();it.hasNext();) {
+            it.next();
+        }
+        end = System.currentTimeMillis();
+        System.out.println("循环" + col.size() + "此for iterator循环用时:\t" + (end - start));
+        // 第四种 iterator while 循环
+        start = System.currentTimeMillis();
+        Iterator itw = col.iterator();
+        while (itw.hasNext()) {
+            itw.next();
+        }
+        end = System.currentTimeMillis();
+        System.out.println("循环" + col.size() + "此iterator while循环用时:\t" + (end - start));
+        // 测试 end 结论：for循环最快，其次是iterator，最后是增强for
     }
 }
 
