d4d57ed77ac03db43f9015d790d71cce226e1ca1
王英男 <disillusion021@gmail.com>
2023-06-07 17:31:00 +0800

    [Add] druid线程池

---
+++ src/com/coding/Test/线程池/DruidUtil.java
@@ -0,0 +1,49 @@
+package com.coding.Test.线程池;
+
+import com.alibaba.druid.pool.DruidDataSourceFactory;
+
+import javax.sql.DataSource;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class DruidUtil {
+    static DataSource ds;
+
+    static {
+        try {
+            Properties properties = new Properties();
+            properties.load(Files.newInputStream(Paths.get("src\\druid.properties")));
+            // 根据配置文件创建数据库连接池
+            ds = DruidDataSourceFactory.createDataSource(properties);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    // 从druid中获取一条数据库连接
+    public static Connection getConnection() throws Exception {
+        return ds.getConnection();
+    }
+
+    // 关闭连接, 在数据库连接池中, 关闭连接, 并不是真正的关闭, 而是将连接归还给数据库连接池
+    public static void close(ResultSet rs, PreparedStatement ps, Connection conn) {
+        try {
+            if (rs != null) {
+                rs.close();
+            }
+            if (ps != null) {
+                ps.close();
+            }
+            if (conn != null) {
+                conn.close();
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
